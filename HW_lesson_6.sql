c66dcc7dac767ff9b5f777491807864ab047c9e2
TatyanaKorolevaa <[200~t310kor233@mail.ru~>
2024-07-26 07:48:51 +0300

    HW lesson_6

--- HW_lesson_6.sql
+++ HW_lesson_6.sql
@@ -3,23 +3,23 @@ use HW_SQL_006;
 
 
 DELIMITER //
-CREATE PROCEDURE sec_counter(num INT)
+CREATE FUNCTION sec_counter(num INT)
+DETERMINISTIC
 BEGIN
-	CASE
-		WHEN num < 60 THEN
+		IF num < 60
 			SELECT CONCAT(num, ' ', 'seconds') AS Result;
-        WHEN num >= 60 AND num < 3600 THEN
+        IF ELSE num >= 60 AND num < 3600
 			SELECT CONCAT_WS(' ', num DIV 60, 'minutes', MOD(num, 60), 'seconds') AS Result;
-        WHEN num >= 3600 AND num < 86400 THEN
+        IF ELSE num >= 3600 AND num < 86400
 			SELECT CONCAT_WS(' ', num DIV 3600, 'hours', MOD(num, 3600) DIV 60, 'minutes', MOD(MOD(num, 3600),60), 'seconds') AS Result;
         ELSE
 			SELECT CONCAT_WS(' ', num DIV 86400, 'days', MOD(num, 86400) DIV 3600, 'hours', MOD(MOD(num, 86400),3600) DIV 60, 'minutes',
                              MOD(MOD(MOD(num, 86400),3600),60), 'seconds') AS Result;
-    END CASE;
+    END IF;
 END//
 
 DELIMITER ;
-CALL sec_counter(123456);  
+SELECT sec_counter(123456);  
 
 
 DELIMITER //
